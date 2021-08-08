58654994688bb9e532c96bb9d3dc0b1d144ce2d8
samvalvi <samuelvalerin@gmail.com>
2021-08-08 14:54:02 -0600

    products reducer

--- src/front/js/redux/productDucks.js
+++ src/front/js/redux/productDucks.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 //Constantes
 const initialData = {
-    products: []
+    products: [],
+    error: ""
 }
 
 //Types
@@ -12,19 +13,27 @@ const GET_PRODUCTS = 'GET_PRODUCTS'
 //Reducers
 export default function productReducer(state = initialData, action) {
     switch (action.type) {
-        case 'GET_PRODUCTS'
-
-            
+        case 'GET_PRODUCTS':
+            return {...state, products: action.payload}
+        case 'ERROR':
+            return {...state, error: action.payload}
+        default:
+            return state  
     }
 }
 
 //Actions
 export const getProductsAction = () => async (dispatch, getState) => {
     try{
-        const resp = await axios.get('http://localhost:3000/api/productos');
+        const resp = await axios.get('http://localhost:3000/api/productos?offset=0&limit=3');
         dispatch({
             type: GET_PRODUCTS,
+            payload: resp
         })
     }catch(error){
+        dispatch({
+            type: ERROR,
+            payload: error
+        })
     }
 }
